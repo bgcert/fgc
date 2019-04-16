dc7bfc786085281ec56aba597c9630a6adeba599
bgcert <info@seminari365.com>
2019-04-16 17:51:12 +0300

    Design start.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,157 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+@section('content')
+<section class="fgc-header">
+    <div class="nav-container">
+        <div class="container">
+            <nav class="main-nav navbar navbar-expand-lg navbar-dark">
+                <a class="navbar-brand" href="#">
+                    <img src="/img/logo.jpg" class="d-inline-block align-top" alt="">
+                </a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">За църквата <span class="sr-only">(current)</span></a>
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="#">За църквата</a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Събития</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Блог</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Контакти</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
                 </div>
-            @endif
+            </nav>
+        </div>
+    </div>
+    <div class="container">
+        <div class="content">
+            <div class="h1">
+                Църква на Пълното Евангелие
+            </div>
+            <p>
+                Църква на Пълното Евангелие е Българска християнска общност, която вярва във всичко, което пише в светото писание – Библията.
+            </p>
+            <p>
+                <a href="#" class="btn btn-danger btn-lg">Свържете се с нас</a>
+                <a href="#" class="btn btn-outline-light btn-lg">Събития</a>
+            </p>
+        </div>
+    </div>
+</section>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+<section class="about-us">
+    <div class="container">
+        <div class="text-center">
+            <div class="h1">За Църквата</div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <div class="col-8">
+                <p>
+                    Църква на Пълното Евангелие е Българска християнска общност, която вярва във всичко, което пише в светото писание – Библията.
+                </p>
+                <p>
+                    Нашето мото е: <strong>"Щом е написано, значи е установено!"</strong>
+                </p>
+                <p>
+                    Вярваме, че Исус Христос е жив и днес върши чудеса! Ние сме приятелска група от млади хора, които искат да отразяват Бога във всяка област от живота ни.
+                </p>
+                <p>
+                    Ако искаш да разбереш и достигнеш до всички обещания, които словото на Бога има за теб и семейството ти, те каним да ни посетиш!
+                </p>
+                <div class="text-center mt-4">
+                    <a href="#" class="btn btn-primary btn-lg">Свържете се с нас</a>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</section>
+<section class="blog">
+    <div class="container">
+        <div class="text-center">
+            <div class="h1">Публикации</div>
+            <div class="fgc-divider"></div>
+        </div>
+
+        <div class="row">
+            @for($i = 0; $i <= 2; $i++)
+            <div class="fgc-blog col-4">
+                <div class="row no-gutters">
+                    <div class="col-12">
+                        <a href="#" class="cover"></a>
+                    </div>
+                    <div class="col-3">
+                        <div class="blog-date">
+                            <div class="day">30</div>
+                            <div class="month">юли</div>
+                        </div>
+                    </div>
+                    <div class="event-content col-9">
+                        <a href="#" class="blog-title">Lorem ipsum dolor sit amet consectetur</a>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Accusamus beatae reprehenderit saepe facilis cumque?
+                        </p>
+                        <div class="separator"></div>
+                        <div class="footer">
+                            <div class="category">
+                                <span>Категория:</span> <a href="#">Слово</a>
+                            </div>
+                            <div class="tags">
+                                <span>Тагове:</span>
+                                <a href="#">мъдрост</a>,
+                                <a href="#">слово</a>,
+                                <a href="#">завет</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endfor
+        </div>
+        <p class="text-center">
+            <a href="#" class="btn btn-danger btn-lg"> Всички публикации</a>
+        </p>
+    </div>
+</section>
+
+<section class="events">
+    <div class="container">
+        <div class="text-center">
+            <div class="h1">Актуални събития</div>
+            <div class="fgc-divider"></div>
+        </div>
+
+        @for($i = 0; $i <= 2; $i++)
+        <div class="fgc-event">
+            <div class="row no-gutters">
+                <div class="col-6 event-date">
+                    <div class="overlay">
+                        <div class="text-center">
+                            <div class="day">30</div>
+                            <div class="month">юли</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="event-content col-6">
+                    <div class="event-title">Lorem ipsum dolor sit amet consectetur</div>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Accusamus beatae reprehenderit saepe facilis cumque?
+                    </p>
+                </div>
+            </div>
+        </div>
+        @endfor
+    </div>
+</section>
+@endsection
