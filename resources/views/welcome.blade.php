f07b57edd56dc6e751e72fbd721245d2f5a62082
bgcert <info@seminari365.com>
2019-04-19 14:26:03 +0300

    Navbar improved.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,8 +22,16 @@
                                 <a class="dropdown-item" href="#">Партньори</a>
                             </div>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Събития</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Служения
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Основно богослужение</a>
+                                <a class="dropdown-item" href="#">Домашни групи</a>
+                                <a class="dropdown-item" href="#">Молитвено събрание</a>
+                                <a class="dropdown-item" href="#">Младежка група</a>
+                            </div>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Блог</a>
